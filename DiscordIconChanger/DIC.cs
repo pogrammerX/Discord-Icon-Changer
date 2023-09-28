19058c40047ade4ece747207ceb5786c45fee575
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2023-09-28 16:55:38 +0200

    Console Only source

---
+++ DiscordIconChanger/DIC.cs
@@ -0,0 +1,57 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// DIC!!! (DiscordIconChanger)
+
+namespace DiscordIconChanger
+{
+    internal class DIC
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
+        public static extern int SHChangeNotify(int wEventId, int uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+        public static void Perform(string discordRoot, string discordAppDir, string icoPath)
+        {
+            string shortcutPath = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord.lnk";
+            if(!Directory.Exists("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc"))
+                Directory.CreateDirectory("C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc");
+            string[] appIcoPaths = { discordRoot + "\\app.ico", discordAppDir + "\\app.ico" };
+
+            Console.WriteLine("Replacing old icon files...");
+            foreach (var appIco in appIcoPaths)
+            {
+                System.IO.File.Move(appIco, Path.GetDirectoryName(appIco) + "\\old_appIco.ico", true);
+                System.IO.File.Copy(icoPath, appIco, true);
+            }
+
+            // Create a Shell object
+            WshShell shell = new WshShell();
+
+            // Load the existing shortcut
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+
+            // Set the new icon path
+            shortcut.IconLocation = appIcoPaths[0];
+            shortcut.TargetPath = discordRoot + "\\Update.exe --processStart Discord.exe";
+
+            Console.WriteLine("Creating new Shortcut");
+            // Save the shortcut
+            shortcut.Save();
+
+            Console.WriteLine("Copying Shortcut...");
+            System.IO.File.Copy(shortcutPath, "C:\\users\\" + Environment.UserName + "\\desktop\\Discord.lnk", true);
+
+            Console.WriteLine("Notifying Shell about Change");
+            // Notify the shell about the change
+            SHChangeNotify(0x08000000, 0x0000, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
---
+++ DiscordIconChanger/Program.cs
@@ -0,0 +1,50 @@
+﻿using DiscordIconChanger;
+using System.Drawing;
+
+class Prpgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("DiscordIconChanger by pogrammerX (https://github.com/pogrammerX/discord-icon-changer)");
+
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Syntax Error!");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("\tdiscordiconchanger <path to ico file>");
+            Console.WriteLine("Or");
+            Console.WriteLine("\tdiscordiconchanger --restore (Restores the default Discord Icon)");
+            return;
+        }
+
+        Console.WriteLine("Locating Discord...");
+        string discordRoot = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\Discord";
+
+        string discordAppDir = "";
+        foreach (var dir in Directory.GetDirectories(discordRoot))
+        {
+            if (Path.GetFileName(dir).StartsWith("app-"))
+                discordAppDir = dir;
+        }
+
+        if(string.IsNullOrWhiteSpace(discordAppDir))
+        {
+            Console.WriteLine("Discord not found!");
+            Console.WriteLine("We've searched in " + discordRoot + " but didn't find it, have you installed Discord?");
+            return;
+        }
+
+        if (args[0] == "--restore")
+        {
+            File.WriteAllBytes(Environment.CurrentDirectory + "\\normal.ico", DiscordIconChanger.Properties.Resources.normal);
+            DIC.Perform(discordRoot, discordAppDir, Environment.CurrentDirectory + "\\normal.ico");
+            File.Delete(Environment.CurrentDirectory + "\\normal.ico");
+            Console.WriteLine("Restored!");
+            return;
+        }
+
+        Console.WriteLine("Changing Icon...");
+        DIC.Perform(discordRoot, discordAppDir, args[0]);
+        Console.WriteLine("Finished!");
+    }
+}
